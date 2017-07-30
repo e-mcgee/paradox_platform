42c3ab49d7172bf6617d2390258727e7815eb16e
e-mcgee <eric.charles.mcgee@gmail.com>
2017-07-30 18:58:59 +0200

    Fixing garage door states v6.

--- index.js
+++ index.js
@@ -892,13 +892,17 @@ ParadoxAccessory.prototype.setDoorState = function (state, callback) {
                                 client.end();
                                 if ( self.garagedooropenerService.readstate == Characteristic.CurrentDoorState.CLOSED) {
                                     self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPENING);
-//                                    setTimeout(function () {
-//                                        self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
-//                                        self.garagedooropenerService.readstate = Characteristic.CurrentDoorState.OPEN;
-//                                    }, DOOROPENTIME);
+                                    setTimeout(function () {
+                                        self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+                                        self.garagedooropenerService.readstate = Characteristic.CurrentDoorState.OPEN;
+                                    }, DOOROPENTIME);
                                }
                                 else {
                                     self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSING);                                    
+                                    setTimeout(function () {
+                                        self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
+                                        self.garagedooropenerService.readstate = Characteristic.CurrentDoorState.CLOSED;
+                                    }, DOOROPENTIME);
                                 }
 //                                self.garagedooropenerService.readstate = state;
 //                                self.garagedooropenerService.setCharacteristic(Characteristic.CurrentDoorState, state);
