0ad81ebde684023b7cbf7886667311e9c91a2929
e-mcgee <e-mcgee@outlook.com>
2018-01-12 01:44:05 +0200

    Setting final state of door #29

--- index.js
+++ index.js
@@ -822,7 +822,7 @@ ParadoxAccessory.prototype.setFinalDoorState = function(callback, state) {
       acc.getCharacteristic(DoorState).setValue(this.targetState);
  //    }
     this.operating = false;
-    callback(null, state);
+    callback(state);
 };
 
 
